aaa3f76e8efe57555b145becea5ca9cfcf973dbf
Manish <manish20171999@gmail.com>
2023-05-15 01:31:07 +0530

    build #2

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./contacts.component.css']
 })
 export class ContactsComponent {
+  constructor() { }
+  increaseSizeGmail() {
+    let icon = document.getElementById("my-icon-gmail");
+    icon?.classList.add("my-icon-large");
+  }
+  increaseSizeGit() {
+    let icon = document.getElementById("my-icon-github");
+    icon?.classList.add("my-icon-large");
+  }
+  increaseSizeLinkedin() {
+    let icon = document.getElementById("my-icon-linkedin");
+    icon?.classList.add("my-icon-large");
+  }
+  ngOnInit(): void { }
 
 }
