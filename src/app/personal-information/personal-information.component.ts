02d4c417a39d47f0f898d3dd702fb33c2607d929
Manish <manish20181998@gmail.com>
2023-11-06 20:14:28 +0530

    experience fixed from 23 to 2.3

--- src/app/personal-information/personal-information.component.ts
+++ src/app/personal-information/personal-information.component.ts
@@ -9,13 +9,13 @@ export class PersonalInformationComponent {
   myData: string[][] = [
     ['Name', 'Manish Baghel'],
     ['DOB', '30/12/1999'],
-    ['Work Exp', '23 Years'],
+    ['Work Exp', '2.3 Years'],
     ['Education', 'B.tech(2021)'],
     ['Interests', 'Cricket'],
   ];
 
   aboutMe: string[] = [
-    'Hi, I am a Software Engineer with 2 years of experience in building angular applications',
+    'Hi, I am a Software Engineer with 2+ years of experience in building angular applications',
     'with a passion for innovation in technology, self-development and ',
     'sharpening my leadership skills in a cutting edge technology area.',
   ];
